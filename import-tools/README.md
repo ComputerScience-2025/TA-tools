81389f2f24cfc9738eceed84f1b9de43074ac93c
roy-y-2023 <153246416+roy-y-2023@users.noreply.github.com>
2025-01-18 01:04:39 -0800

    add team to repository

--- import-tools/README.md
+++ import-tools/README.md
@@ -1,15 +1,25 @@
 # import-tools
 
-To install dependencies:
 
+To install dependencies:
 ```bash
 bun install
 ```
 
-To run:
-
+To create repositories from class roster:
 ```bash
-bun run src/index.ts
+bun run setup-repos
+```
+
+To grant access to repositories based on team:
+```bash
+bun run add-collaborators
+```
+
+To import submission files from Canvas:
+First need to create a `./submissions` directory and download the submissions from Canvas into that directory. Then run:
+```bash
+bun run import-submissions
 ```
 
 This project was created using `bun init` in bun v1.1.43. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- import-tools/src/add-collaborator.ts
+++ import-tools/src/add-collaborator.ts
@@ -4,6 +4,12 @@ import {octokit} from "./service.ts";
 import {Naming} from "./helper/naming.ts";
 
 
+enum InviteeType {
+    User = "user",
+    Team = "team",
+}
+
+
 console.log("Add a collaborator to repositories based on roster");
 
 let roster = await askForRosterFile();
@@ -12,9 +18,10 @@ console.log(roster);
 let organization = Config.getGitHubOrgName();
 let courseName = prompt("Enter the course name: ") ?? "YOU_FORGOT_TO_ENTER_A_COURSE_NAME";
 let sectionName = prompt("Enter the section name: ") ?? "YOU_FORGOT_TO_ENTER_A_SECTION_NAME";
-let collaborator = prompt("Enter the collaborator's GitHub username: ") ?? "YOU_FORGOT_TO_ENTER_A_GITHUB_USERNAME";
+let collaboratorType = prompt("Enter the collaborator type (user/team): ") ?? "user";
+let collaboratorName = prompt("Enter the collaborator's GitHub username: ") ?? "YOU_FORGOT_TO_ENTER_A_GITHUB_USERNAME";
 let collaboratorRole = prompt("Enter the collaborator's role (pull/triage/push/maintain/admin): ") ?? "pull";
-console.log(`Org: "${organization}" Course name: "${courseName}", section name: "${sectionName}", collaborator: "${collaborator}", role: "${collaboratorRole}"`);
+console.log(`Org: "${organization}" Course name: "${courseName}", section name: "${sectionName}", collaborator: "${collaboratorName}", role: "${collaboratorRole}"`);
 
 let confirmation = prompt("Is this correct? (yes/no): ");
 if (confirmation !== "yes") {
@@ -26,15 +33,37 @@ for (let student of roster.students) {
     console.log(`Student: ${student.fullName} (${student.userName})`);
     let repoName = Naming.makeRepositoryName({courseName, sectionName, personName: student.userName, personID: student.id});
     
-    try {
-        await octokit.rest.repos.addCollaborator({
-            owner: organization,
-            repo: repoName,
-            username: collaborator,
-            permission: collaboratorRole
-        });
-        console.log(`Added ${collaborator} to ${repoName}`);
-    } catch (error) {
-        console.error(`Failed to add ${collaborator} to ${repoName}:`, error);
+    switch (collaboratorType) {
+        case InviteeType.User:
+            try {
+                await octokit.rest.repos.addCollaborator({
+                    owner: organization,
+                    repo: repoName,
+                    username: collaboratorName,
+                    permission: collaboratorRole
+                });
+                console.log(`Added ${collaboratorName} to ${repoName}`);
+            } catch (error) {
+                console.error(`Failed to add ${collaboratorName} to ${repoName}:`, error);
+            }
+            break;
+        case InviteeType.Team:
+            try {
+                await octokit.rest.teams.addOrUpdateRepoPermissionsInOrg({
+                    org: organization,
+                    team_slug: collaboratorName,
+                    owner: organization,
+                    repo: repoName,
+                    permission: collaboratorRole
+                });
+                console.log(`Added team ${collaboratorName} to ${repoName}`);
+            } catch (e) {
+                console.error(`Failed to add team ${collaboratorName} to ${repoName}:`, e);
+            }
+            break;
+        default:
+            console.error(`Invalid collaborator type "${collaboratorType}"`);
+            break;
     }
+    
 }
