51e055276002cd83c39c409f74008f97bb21593c
roy-y-2023 <153246416+roy-y-2023@users.noreply.github.com>
2025-11-21 15:00:46 -0800

    dashboard: links opener

--- ta-dashboard/src/routes/+page.svelte
+++ ta-dashboard/src/routes/+page.svelte
@@ -4,5 +4,6 @@
     <ul>
         <li><a href="/settings">Settings</a></li>
         <li><a href="/tools/comments-calculator">Comments Score Calculator</a></li>
+        <li><a href="/tools/links-opener">Links Opener</a></li>
     </ul>
 </div>
---
+++ ta-dashboard/src/routes/tools/links-opener/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import {toast} from "@zerodevx/svelte-toast";
+    import {persisted} from "svelte-persisted-store";
+
+    let repositoryBaseURL = $state("");
+    let pullRequestNumber = $state("");
+    let branchName = $state("");
+
+    function openToNewTal(url: string) {
+        window.open(url, '_blank');
+    }
+
+    function openLinksButton() {
+        if (!repositoryBaseURL) {
+            toast.push("Repository Base URL is required.");
+            return;
+        }
+
+        if (pullRequestNumber) {
+            const prVCSWebURL = `${repositoryBaseURL.replace("github.com", "github.dev")}/pull/${pullRequestNumber}`;
+            const prFilesTabURL = `${repositoryBaseURL}/pull/${pullRequestNumber}/files`;
+            openToNewTal(prVCSWebURL);
+            openToNewTal(prFilesTabURL);
+        }
+
+        if (branchName) {
+            const branchURL = `${repositoryBaseURL}/tree/${branchName}`;
+            openToNewTal(branchURL);
+        }
+    }
+</script>
+
+<h1 class="title">Links Opener</h1>
+
+<h4 class="subtitle">Note: it will open multiple new tabs, please make sure to "Allow Popup"</h4>
+
+<p>
+    Repo URL: <input type="text" bind:value={repositoryBaseURL} size="100" />
+</p>
+<p>
+    Pull Request Number: <input type="text" bind:value={pullRequestNumber} />
+</p>
+<p>
+    Branch Name: <input type="text" bind:value={branchName} />
+</p>
+
+<button class="button" onclick={openLinksButton}>Open Links</button>
